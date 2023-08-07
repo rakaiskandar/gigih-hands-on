0071049c8453071f81a33e537d5f08fd540feda9
rakaiskandar <iskandarraka4@gmail.com>
2023-08-07 20:37:47 +0700

    modify: update dependencies

--- 2-3_rest-api-express/2-3_homework/app.js
+++ 2-3_rest-api-express/2-3_homework/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
 const port = 3000;
 
 const songsRouter = require('./routes/songsRoute');
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', songsRouter);
--- 2-3_rest-api-express/2-3_homework/models/songs.js
+++ 2-3_rest-api-express/2-3_homework/models/songs.js
@@ -2,18 +2,25 @@ const { generateRandomId } = require("../utils/utilities");
 
 const playlist = [
     {   
-        id: "9572122141",
-        title: "Chamber Of Reflection",
-        artist: ["Mac Demarco"],
-        total: 10,
-        url: "https://open.spotify.com/track/7H7NyZ3G075GqPx2evsfeb?si=8d889d7a95b94b88",
-    },
-    {   
         id: "957212277",
         title: "Always",
         artist: ["Daniel Caesar"],
         total: 3,
         url: "https://open.spotify.com/track/71XJNtOdwmL1UWOQQvsKoH?si=c0ab9e044e334f2e",
+    },
+    {   
+        id: "957212256",
+        title: "Sunflower",
+        artist: ["Rex Orange County"],
+        total: 1,
+        url: "https://open.spotify.com/track/71XJNtOdwmL1UWOQQvsKoH?si=c0ab9e044e334f2e",
+    },
+    {   
+        id: "9572122199",
+        title: "Chamber Of Reflection",
+        artist: ["Mac Demarco"],
+        total: 10,
+        url: "https://open.spotify.com/track/7H7NyZ3G075GqPx2evsfeb?si=8d889d7a95b94b88",
     }
 ]
 
